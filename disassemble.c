ffccf8d85ff51bb429efabb5568d10c38efd57a1
Nico01 <nicola.onorata@gmail.com>
2014-12-27 11:49:02 +0100

    cleanup

--- disassemble.c
+++ disassemble.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <inttypes.h>
-#include <capstone/capstone.h>
 
 #include "disassemble.h"
 #include "elf_32.h"
@@ -18,15 +17,14 @@ size_t file_size(const char *fileName)
     return -1; 
 }
 
-void disasm_x86_64(Elf64_Ehdr *ehdr, uint32_t offset, size_t size, uint8_t *data)
+void disasm(uint64_t addr, uint32_t offset, size_t size, cs_arch arch, cs_mode mode, uint8_t *data)
 {
     int i;
     csh handle = 0;
-    uint64_t address;
     cs_insn *insn;
     const uint8_t *code;
 
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+    if (cs_open(arch, mode, &handle) != CS_ERR_OK) {
         printf("ERROR: Failed to initialize engine!\n");
         exit(EXIT_FAILURE);
     }
@@ -37,11 +35,10 @@ void disasm_x86_64(Elf64_Ehdr *ehdr, uint32_t offset, size_t size, uint8_t *data
     printf("Disassembly of section .text:\n");
 
     for (i = offset; i < offset + size; i++) {
-        address = ehdr->e_entry;
         code = &data[i];
         insn = cs_malloc(handle);
 
-        while(cs_disasm_iter(handle, &code, &size, &address, insn)) {
+        while(cs_disasm_iter(handle, &code, &size, &addr, insn)) {
             printf("0x%"PRIx64":\t%s\t\t%s\n", insn->address, insn->mnemonic, insn->op_str);
         }
         cs_free(insn, 1);
@@ -54,38 +51,7 @@ void elf_disasm_64(Elf64_Ehdr *ehdr, Elf64_Shdr *shdr, char *str_table, uint8_t 
     uint32_t offset = get_section_offset_64(ehdr, shdr, str_table, ".text");
     size_t size = get_section_size_64(ehdr, shdr, str_table, ".text");
 
-    disasm_x86_64(ehdr, offset, size, data);
-}
-
-void disasm_x86(Elf32_Ehdr *ehdr, uint32_t offset, size_t size, uint8_t *data)
-{
-    int i;
-    csh handle = 0;
-    uint64_t address;
-    cs_insn *insn;
-    const uint8_t *code;
-
-    if (cs_open(CS_ARCH_X86, CS_MODE_32, &handle) != CS_ERR_OK) {
-        printf("ERROR: Failed to initialize engine!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    cs_option(handle, CS_OPT_SKIPDATA, CS_OPT_ON);
-    cs_option(handle, CS_OPT_SYNTAX, CS_OPT_SYNTAX_ATT);   
-
-    printf("Disassembly of section .text:\n");
-
-    for (i = offset; i < offset + size; i++) {
-        address = ehdr->e_entry;
-        code = &data[i];
-        insn = cs_malloc(handle);
-
-        while(cs_disasm_iter(handle, &code, &size, &address, insn)) {
-            printf("0x%"PRIx64":\t%s\t\t%s\n", insn->address, insn->mnemonic, insn->op_str);
-        }
-        cs_free(insn, 1);
-    }
-    cs_close(&handle);
+    disasm(ehdr->e_entry, offset, size, CS_ARCH_X86, CS_MODE_64, data);
 }
 
 void elf_disasm_32(Elf32_Ehdr *ehdr, Elf32_Shdr *shdr, char *str_table, uint8_t *data)
@@ -93,5 +59,5 @@ void elf_disasm_32(Elf32_Ehdr *ehdr, Elf32_Shdr *shdr, char *str_table, uint8_t 
     uint32_t offset = get_section_offset_32(ehdr, shdr, str_table, ".text");
     size_t size = get_section_size_32(ehdr, shdr, str_table, ".text");
 
-    disasm_x86(ehdr, offset, size, data);
+    disasm(ehdr->e_entry, offset, size, CS_ARCH_X86, CS_MODE_32, data);
 }
--- disassemble.h
+++ disassemble.h
@@ -1,17 +1,11 @@
 #include <elf.h>
 #include <stdlib.h>
+#include <capstone/capstone.h>
 
 size_t file_size(const char *fileName);
 
-/* 64 bit functions */
-
-void disasm_x86_64(Elf64_Ehdr *ehdr, uint32_t offset, size_t size, uint8_t *data);
+void disasm(uint64_t addr, uint32_t offset, size_t size, cs_arch arch, cs_mode mode, uint8_t *data);
 
 void elf_disasm_64(Elf64_Ehdr *ehdr, Elf64_Shdr *shdr, char *str_table, uint8_t *data);
-
-
-/* 32 bit functions */
-
-void disasm_x86(Elf32_Ehdr *ehdr, uint32_t offset, size_t size, uint8_t *data);
 
 void elf_disasm_32(Elf32_Ehdr *ehdr, Elf32_Shdr *shdr, char *str_table, uint8_t *data);
